19ca3dda6f41b719ce4d2b52bebe7f26aa279532
Narug1fps <144721920+Narug1fps@users.noreply.github.com>
2025-10-17 23:27:44 -0300

    Add back navigation button to portfolio page
    Added a button to navigate back to the home page.

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -16,6 +16,15 @@ export default function Portfolio() {
 
   return (
    <section className="relative bg-white py-16 ">
+
+      <div className="container mx-auto px-6 ">
+        <Link href="/">
+          <Button variant="ghost" className="mb-8 border-2 hover:border-secondary border-transparent hover:bg-  text-muted-foreground cursor-pointer">
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            Voltar para a pagina inicial
+          </Button>
+        </Link>
+      </div>
   {/* Header */}
   <div className="container mx-auto px-4   text-center">
     <div className="inline-flex items-center gap-2 bg-secondary/20 text-secondary-foreground px-4 py-2 rounded-full text-sm font-medium mb-6">
